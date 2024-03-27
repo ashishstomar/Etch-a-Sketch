8a7040ab27935e302576421cfd3eb748c9c50999
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-03-27 23:45:16 +0530

    add:  button functionality

--- script.js
+++ script.js
@@ -1,46 +1,91 @@
 const body = document.querySelector("body")
 const container = document.querySelector(".container");
+
+const header = document.querySelector(".header")
+header.addEventListener("mouseover", () => {
+header.style.color = randomRGB();
+
+})
+
+const headerBtn = document.createElement("div")
 const btnrgb = document.createElement("button");
 const btnBlack = document.createElement("button");
 const userInput = document.createElement("input");
-userInput.innerText = "Grid Size";
+userInput.placeholder ='input';
+const clear = document.createElement("button");
+const erase = document.createElement("button");
+
+headerBtn.classList.add("headerBtn");
 userInput.classList.add("userInput");
+clear.classList.add("btn")
+erase.classList.add("btn")
+btnBlack.classList.add("btn")
+btnrgb.classList.add("btn")
 
+userInput.innerText = "Grid Size";
+clear.textContent = "Clear";
+erase.textContent = "Erase";
 btnrgb.innerText = "RGB";
-btnBlack.innerText = "black";
-btnBlack.classList.add("btnBlack")
-btnrgb.classList.add("btnrgb")
-body.appendChild(btnrgb);
-body.appendChild(btnBlack);
-body.appendChild(userInput)
+btnBlack.innerText = "Mono";
+body.insertBefore(headerBtn, container);
+headerBtn.appendChild(btnrgb);
+headerBtn.appendChild(btnBlack);
+headerBtn.appendChild(userInput)
+headerBtn.appendChild(erase);
+headerBtn.appendChild(clear);
 
 
-function squareGrid(row, column) {
+function squareGrid(rowxColumn) {
 
-    for(let i = 0; i < row; i++ )
+    for(let i = 0; i < rowxColumn.toExponential(2); i++ )
     {
         const row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
-
-        for (let j = 0; j < column; j++)
+        console.log(row)
+        for (let j = 0; j < rowxColumn.toExponential(2); j++)
         {
             const column = document.createElement("div");
             column.classList.add("column");
             row.appendChild(column);
+
+            btnBlack.addEventListener("click", () => {
+                column.addEventListener("mouseover", () => {
+                column.style.backgroundColor = "black";
+            })})
+
+            btnrgb.addEventListener("click", () => {
+                column.addEventListener("mouseover", () => {
+                column.style.backgroundColor = randomRGB();
+            })})
+
+            erase.addEventListener("click", () => {
+                column.addEventListener("mouseover", () => {
+                column.style.backgroundColor = "white";
+            })})
+            
         }
     }
+    clearAll();
 }
 
+function clearAll() {
+    clear.addEventListener("click", () => {
+        window.location.reload();
+        return false;
+    })
+}
 
-squareGrid(16, 16);
+function randomRGB() {
+    let color = [];
+    for(let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return  'rgb(' + color. join(', ') + ')';
+    
+}
 
-
-
-
-
-
-
+squareGrid(16);
 
 
 
