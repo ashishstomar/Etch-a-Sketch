dbf99a9a4a448ed96978165f67e4a91fed54dc62
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-03-28 12:13:31 +0530

    user input for grid size between 1 to 99

--- script.js
+++ script.js
@@ -4,19 +4,40 @@ const container = document.querySelector(".container");
 const header = document.querySelector(".header")
 header.addEventListener("mouseover", () => {
 header.style.color = randomRGB();
-
 })
 
 const headerBtn = document.createElement("div")
 const btnrgb = document.createElement("button");
 const btnBlack = document.createElement("button");
-const userInput = document.createElement("input");
-userInput.placeholder ='input';
 const clear = document.createElement("button");
 const erase = document.createElement("button");
 
+const userInput = document.createElement("input");
+userInput.placeholder ='input';
+userInput.setAttribute("type", "number");
+userInput.setAttribute("id", "userInput");
+const userInputBtn = document.createElement("button");
+userInputBtn.classList.add("UserInputbtn");
+userInputBtn.textContent = "Go";
+
+
+userInputBtn.addEventListener("click", () => {
+    let gridSizeValue = document.getElementById("userInput").value;
+    if ( gridSizeValue < 1 || gridSizeValue > 99) {
+        alert("Please enter a value between 1 to 99");
+    }
+    else {
+        //remove Existing divs in the container is Any
+        const row = document.querySelectorAll('.row');
+        row.forEach(row => row.remove());
+
+        //then create new grid upon user input
+        squareGrid(gridSizeValue);
+    }    
+})
+
+
 headerBtn.classList.add("headerBtn");
-userInput.classList.add("userInput");
 clear.classList.add("btn")
 erase.classList.add("btn")
 btnBlack.classList.add("btn")
@@ -30,20 +51,21 @@ btnBlack.innerText = "Mono";
 body.insertBefore(headerBtn, container);
 headerBtn.appendChild(btnrgb);
 headerBtn.appendChild(btnBlack);
-headerBtn.appendChild(userInput)
 headerBtn.appendChild(erase);
 headerBtn.appendChild(clear);
+headerBtn.appendChild(userInput)
+headerBtn.appendChild(userInputBtn);
 
 
 function squareGrid(rowxColumn) {
-
-    for(let i = 0; i < rowxColumn.toExponential(2); i++ )
+    //loop to append one column div easch iteration
+    for(let i = 0; i < rowxColumn; i++ )
     {
         const row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
-        console.log(row)
-        for (let j = 0; j < rowxColumn.toExponential(2); j++)
+        //loop to append row divs inside the column div 
+        for (let j = 0; j < rowxColumn; j++)
         {
             const column = document.createElement("div");
             column.classList.add("column");
@@ -69,10 +91,11 @@ function squareGrid(rowxColumn) {
     clearAll();
 }
 
+//reset the canvas
 function clearAll() {
     clear.addEventListener("click", () => {
-        window.location.reload();
-        return false;
+        const column = document.querySelectorAll('.column');
+        column.forEach( (column) => column.style.backgroundColor = "white");
     })
 }
 
@@ -86,43 +109,3 @@ function randomRGB() {
 }
 
 squareGrid(16);
-
-
-
-
-
-
-/*
-
-const container = document.querySelector(".container");
-const row = document.createElement("div");
-row.classList.add("row");
-const column = document.createElement("div");
-column.classList.add("column");
-
-container.appendChild(row);
-container.appendChild(column);
-
-for(let i = 0; i <= 16; i++) {
-    const rowDivs = document.createElement("div");
-    rowDivs.classList.add("rowDivs");
-    row.appendChild(rowDivs);
-    for(let j = 1; j <= 16; j++) {
-        const columnDivs = document.createElement("div");
-        columnDivs.classList.add("columnDivs");
-        column.appendChild(columnDivs);
-    }
-}
-
-//const column = document.querySelector(".columnDivs");
-//const row = document.querySelector(".rowDivs");
-
-row.addEventListener('mouseover', function() {
-    row.style.backgroundColor = 'rgb(255, 0, 0)';
-  });
-
-  column.addEventListener('mouseover', function() {
-    column.style.backgroundColor = 'rgb(255, 0, 0)';
-  });
-
-*/
